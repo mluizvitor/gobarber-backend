48efe1e24e0761b98977d90a2e68b0a44439cf05
Vitor Monteiro <mluizvitor@gmail.com>
2020-05-06 21:54:26 -0300

    feat: added debug settings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello Developero!' }),
-);
+routes.post('/user', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
 
 export default routes;
