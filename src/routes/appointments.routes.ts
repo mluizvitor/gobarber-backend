1b3e2b099ca046d7c4bea53b91a127d46209c509
Vitor Monteiro <mluizvitor@gmail.com>
2020-05-06 21:54:58 -0300

    feat: added Appointment route, repository and model

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRespository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRespository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'This appointment is already booked!' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
